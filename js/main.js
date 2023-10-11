f241957a3a8c736254992f908027a57cb0e86d10
Prasan Kumar <56994366+prasanmoonraft@users.noreply.github.com>
2023-10-11 05:53:16 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
 // For the humburger menu and close icon
 document.getElementById("header_wrapper_mobile_toggle").onclick = function () {
-    document.getElementById("header_wrapper_mobile").classList.toggle("menu_toggle");
-    document.getElementById("main").classList.toggle("toggle_main");
-  };
-  
-  // For the header accordian to open and close
-  var arrow = document.getElementsByClassName("main-nav_wrapper");
-  var dropdown = document.getElementsByClassName("main-nav__dropdown");
-  
-  for (var i = 0; i < arrow.length; i++) {
-    arrow[i].onclick = function() {
-          var setClasses = !this.classList.contains('active');
-          setClass(arrow, 'active', 'remove');
-          setClass(dropdown, 'show', 'remove');
-          
-             if (setClasses) {
-              this.classList.toggle("active");
-              this.nextElementSibling.nextElementSibling.classList.toggle("show");
-          }
-      }
+  document.getElementById("header_wrapper_mobile").classList.toggle("menu_toggle");
+  document.getElementById("main").classList.toggle("toggle_main");
+  var spokenOnlyText = document.getElementById("spokenonlytext");
+  if (spokenOnlyText.innerHTML === "Open main menu") {
+    spokenOnlyText.innerHTML = "Close main menu";
+  } else {
+    spokenOnlyText.innerHTML = "Open main menu";
   }
-  
-  function setClass(element, className, fnName) {
-      for (var i = 0; i < element.length; i++) {
-        element[i].classList[fnName](className);
-      }
+};
+
+// For the header accordian to open and close
+var arrow = document.getElementsByClassName("main-nav_wrapper");
+var dropdown = document.getElementsByClassName("main-nav__dropdown");
+
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].onclick = function () {
+    var setClasses = !this.classList.contains('active');
+    setClass(arrow, 'active', 'remove');
+    setClass(dropdown, 'show', 'remove');
+
+    if (setClasses) {
+      this.classList.toggle("active");
+      this.nextElementSibling.nextElementSibling.classList.toggle("show");
+    }
   }
+}
+
+function setClass(element, className, fnName) {
+  for (var i = 0; i < element.length; i++) {
+    element[i].classList[fnName](className);
+  }
+}
